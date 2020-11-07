3aa03625b4dec1da8c469db4afddde3035e991e1
stscoundrel <sampo@pixels.fi>
2020-11-07 17:24:16 +0200

    Add argument errors & test case

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-const hasProperty = (object, property) => Object.prototype.hasOwnProperty.call(object, property)
+const hasProperty = (object, property) => {
+  if (object === undefined || property === undefined) throw new Error('Missing argument: expects an object and string property key')
+
+  return Object.prototype.hasOwnProperty.call(object, property)
+}
 
 module.exports = {
   hasProperty,
--- tests/spyrjari.test.js
+++ tests/spyrjari.test.js
@@ -1,6 +1,12 @@
 const { hasProperty } = require('../index.js')
 
 describe('Spyrjari: hasProperty', () => {
+  test('Throws error on missing argument', () => {
+    expect(() => {
+      hasProperty()
+    }).toThrow('Missing argument: expects an object and string property key')
+  })
+
   test('Return boolean values', () => {
     const obj = {}
     const result = hasProperty(obj, null)
@@ -10,10 +16,14 @@ describe('Spyrjari: hasProperty', () => {
 
   test('Detects object properties', () => {
     const obj = { id: 666, name: 'Number of the Beast' }
-    const result = hasProperty(obj, 'id')
-    const result2 = hasProperty(obj, 'author')
 
-    expect(result).toBeTruthy()
-    expect(result2).toBeFalsy()
+    expect(hasProperty(obj, 'id')).toBeTruthy()
+    expect(hasProperty(obj, 'author')).toBeFalsy()
+  })
+
+  test('Return false on non-objects', () => {
+    expect(hasProperty('object', 'id')).toBeFalsy()
+    expect(hasProperty(['id'], 'id')).toBeFalsy()
+    expect(hasProperty(5, 'id')).toBeFalsy()
   })
 })
