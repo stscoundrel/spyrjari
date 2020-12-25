b31c8d5a791a54f6b32b44c1feefc7654b2e71d2
stscoundrel <sampo@pixels.fi>
2020-12-25 10:53:40 +0200

    Rewrite to typescript

---
+++ src/spyrjari.ts
@@ -0,0 +1,9 @@
+export function hasProperty(entry: any, property = ''): boolean {
+  if (entry === undefined || property === undefined) throw new Error('Missing argument: expects an object and string property key');
+
+  return Object.prototype.hasOwnProperty.call(entry, property);
+}
+
+export default {
+  hasProperty,
+};
